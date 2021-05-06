d591d3a3ce5cb3ca7e53171aee23725afaae5235
ckfranz <cfranz@uwaterloo.ca>
2021-05-06 17:44:34 -0400

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,6 @@
+// api key: c317c6e7-4594-49cc-a16c-d5d3984acb1b
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'c317c6e7-4594-49cc-a16c-d5d3984acb1b';
+export const MODAL_CLOSE_SEC = 2.5;
